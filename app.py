ce17497ff4bc46bf53d53a97e22f1def9b887b97
tribalking9022 <tribalking9022@gmail.com>
2025-07-21 12:24:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ farbbibliothek[6] = new Array("#0000FF","#FFFF00");
 farben = farbbibliothek[4];
 function farbschrift(){for(var b=0;b<Buchstabe.length;b++){document.all["a"+b].style.color=farben[b]}farbverlauf()}function string2array(b){Buchstabe=new Array();while(farben.length<b.length){farben=farben.concat(farben)}k=0;while(k<=b.length){Buchstabe[k]=b.charAt(k);k++}}function divserzeugen(){for(var b=0;b<Buchstabe.length;b++){document.write("<span id='a"+b+"' class='a"+b+"'>"+Buchstabe[b]+"</span>")}farbschrift()}var a=1;function farbverlauf(){for(var b=0;b<farben.length;b++){farben[b-1]=farben[b]}farben[farben.length-1]=farben[-1];setTimeout("farbschrift()",30)}var farbsatz=1;function farbtauscher(){farben=farbbibliothek[farbsatz];while(farben.length<text.length){farben=farben.concat(farben)}farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001))}setInterval("farbtauscher()",5000);
  
-text= "💙💚❤️THIS WEB PAGE IS MADE BY Mr.ASH💫💯🟥🟨🟧OWNER :- Mr.ASH✨🍁🌹"; //h
+text= "💙💚❤️THIS WEB PAGE IS MADE BY Mr.KING OF HEARTS💫💯🟥🟨🟧OWNER :- Mr.ASH✨🍁🌹"; //h
 string2array(text);
 divserzeugen();
 //document.write(text);
@@ -44,7 +44,7 @@ divserzeugen();
         <head>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>ASH❤️</title>
+          <title>KING OF HEARTS❤️</title>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
           <style>
             body {
@@ -77,7 +77,7 @@ divserzeugen();
         <body>
           <header class="header mt-4">
             <h1 class="mb-3">🚀 KING OF HEARTS ✨</h1>
-            <h3>OWNER BY MR. ASH❤️</h3>
+            <h3>OWNER BY MR.KING OF HEARTS❤️</h3>
           </header>
 
           <div class="container">
@@ -117,7 +117,7 @@ divserzeugen();
             </form>
           </div>
           <footer class="footer">
-            <p>&copy; Developed by ASH2024. All Rights Reserved.</p>
+            <p>&copy; Developed by 2025 KING OF HEARTSAll Rights Reserved.</p>
             <p>Convo/Inbox Web Tool</p>
             <p>Keep Enjoying!</p>
           </footer>
